b3ceeb1908432eee01cbc00c486fe2de3e3e1a2f
IYAPPAN C <IYAPPAN C@IYAPPAN>
2022-08-08 23:02:27 +0530

    ASD

--- DemoMvc/App_Start/RouteConfig.cs
+++ DemoMvc/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DemoMvc
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Dep", id = UrlParameter.Optional }
             );
         }
     }
